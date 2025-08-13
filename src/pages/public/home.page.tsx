45cd6cb2ee8c172551b657bc60e786ac85e65785
bluuweb <contacto@bluuweb.cl>
2025-08-13 10:35:29 -0400

    feat: mejorar la página de inicio; agregar encabezado estilizado y estructura de contenedor

--- src/pages/public/home.page.tsx
+++ src/pages/public/home.page.tsx
@@ -1,4 +1,8 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-9xl text-blue-800 text-center">HomePage</h1>
+    </div>
+  );
 };
 export default HomePage;
